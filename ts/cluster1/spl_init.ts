e375d2e0ac651677b991bbb1e91ddb3daa5c5153
maiconcrespo <maiconviniciuscrespo@gmail.com>
2025-07-03 00:19:55 +0200

    init-metadata-mint

--- ts/cluster1/spl_init.ts
+++ ts/cluster1/spl_init.ts
@@ -1,6 +1,6 @@
 import { Keypair, Connection, Commitment } from "@solana/web3.js";
-import { createMint } from '@solana/spl-token';
-import wallet from "../turbin3-wallet.json"
+import { createMint } from "@solana/spl-token";
+import wallet from "./wallet/turbin3-wallet.json";
 
 // Import our keypair from the wallet file
 const keypair = Keypair.fromSecretKey(new Uint8Array(wallet));
@@ -10,10 +10,18 @@ const commitment: Commitment = "confirmed";
 const connection = new Connection("https://api.devnet.solana.com", commitment);
 
 (async () => {
-    try {
-        // Start here
-        // const mint = ???
-    } catch(error) {
-        console.log(`Oops, something went wrong: ${error}`)
-    }
-})()
+  try {
+    // Start here
+    const mint = await createMint(
+      connection,
+      keypair,
+      keypair.publicKey,
+      null,
+      6,
+    );
+    console.log(`Successfull created a mint: ${mint}`);
+    //8XTbWy3htpaGwVvkEF6Us9y2vJX63QaFap8xFJp4N1fF
+  } catch (error) {
+    console.log(`Oops, something went wrong: ${error}`);
+  }
+})();
